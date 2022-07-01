3d0aa66424f15c3307d9de2acf49da8b2366997b
V. M. Valverde <93843339+victorvalverdem@users.noreply.github.com>
2022-06-30 23:53:35 -0500

    Actualizacion y renombre de Transporte a Vehiculo
    Correcciones de profesora

--- Servicios/Vehiculo.java
+++ Servicios/Vehiculo.java
@@ -1,12 +1,12 @@
 package Servicios;
-public class Transporte{
+public class Vehiculo{
     private String idVehiculo;
     private String marca;
     private String modelo;
     private String placa;
     private String color;
     private int capacidad;
-    private boolean estado;
+    private Disponibilidad estado;
     private double alquilerXdia;
     private TipoConduccion tipo;
     public static int transportesReservados;
