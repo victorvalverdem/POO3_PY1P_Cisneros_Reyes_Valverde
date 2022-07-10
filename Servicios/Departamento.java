440c7b54ae382563abf9ced380ac482d111345c5
Steven Cisneros <108556184+stevenACV@users.noreply.github.com>
2022-07-10 03:42:05 +0000

    Implementar la clase Departamento e incluir sus atributos y métodos

---
+++ Servicios/Departamento.java
@@ -0,0 +1,34 @@
+package Servicios;
+
+public class Departamento{
+
+  private int numHabitaciones;
+  private boolean inWifi;
+  private boolean inPiscina;
+
+
+  // GETTERS Y SETTERS
+  public int getNumHabitaciones(){
+    return this.numHabitaciones;
+  }
+  public void setNumHabitaciones(int numHabitaciones){
+    this.numHabitaciones = numHabitaciones;
+  }
+
+  public boolean getInWifi(){
+    return this.inWifi;
+  }
+  public void setInWifi(boolean inWifi){
+    this.inWifi = inWifi;
+  }
+
+  public boolean getInPiscina(){
+    return this.inPiscina;
+  }
+  public void setInPiscina(boolean inPiscina){
+    this.inPiscina = inPiscina;
+  }
+
+  
+  
+}
