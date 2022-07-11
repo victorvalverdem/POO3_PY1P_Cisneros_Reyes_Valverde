4a97270b62ada5c6dc2ae4932b519a3d9b4340e1
V. M. Valverde <93843339+victorvalverdem@users.noreply.github.com>
2022-07-11 01:11:07 +0000

    generarVehiculos Update -metodo para que reservarTransporte lo utilice

--- Servicios/Servicio.java
+++ Servicios/Servicio.java
@@ -28,6 +28,39 @@ public class Servicio {
     }
 
     public ArrayList<Vehiculo> generarVehiculos(){
+        ArrayList<String> lineasVehiculos //= lo que recibe de la funcion lectora
+        //2. Creacion de ArrayList que recibe los objetos de las clases pertinentes
+        ArrayList<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+        //3. Por cada elemento String del arrayList de String
+        for(String lineaVehiculo:lineasVehiculos){
+            String[] elementos = lineaVehiculo.split(",");
+            //códigoVehículo,marca,modelo,placa,color,capacidad,estado,costo por día,tipo transmision
+            String id = elementos[0];
+            String marca = elementos[1];
+            String modelo = elementos[2];
+            String placa = elementos[3];
+            String color = elementos[4];
+            int capacidad = Integer.parseInt(elementos[5]);
+            String estado = elementos[6]; // MEJORAR ENUM
+            int costo = Integer.parseInt(elementos[7]);
+            String tipoTransmision = elementos[8]; //MEJORAR ENUM
+            
+            //3.1 String[] y Split 3.2 Parseo y clasifico cada division de la linea[] 3.3 Creacion del objeto ENTRETENIMIENTO y seteo con lo clasificado
+            Vehiculo v = new Vehiculo();
+            v.setIdVehiculo(id);
+            v.setMarca(marca);
+            v.setModelo(modelo);
+            v.setPlaca(placa);
+            v.setColor(color);
+            v.setCapacidad(capacidad);
+            v.setEstado(estado);
+            v.setAlquilerXdia(costo);
+            v.setTipoTransmision(tipoTransmision);
+
+            vehiculos.add(v);            
+        }
+
+        return vehiculos;
         
     }
     
--- Servicios/Vehiculo.java
+++ Servicios/Vehiculo.java
@@ -9,11 +9,50 @@ public class Vehiculo{
     private Disponibilidad estado;
     private double alquilerXdia;
     private TipoConduccion tipo;
+    
     public static int transportesReservados;
 
     public void mostrarDatos(){
         //Next
     }
+
+    public void setIdVehiculo(String id){
+        this.idVehiculo = id;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public void setModelo(String id){
+        this.modelo = id;
+    }
+
+    public void setPlaca(String id){
+        this.placa = id;
+    }
+
+    public void setColor(String id){
+        this.color = id;
+    }
+
+    public void setAlquilerXdia(int id){
+        this.alquilerXdia = id;
+    }
+
+    public void setEstado(Disponibilidad id){
+        this.estado = id;
+    }
+
+    public void setCapacidad(int id){
+        this.capacidad = id;
+    }
+
+    public void setTipoConduccion(TipoConduccion t){
+        this.tipoConduccion = t;
+    }
+    
+    
 
     public int getCapacidad(){
         return this.capacidad;
